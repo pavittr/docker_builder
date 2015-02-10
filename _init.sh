acdd91cf72d391bd47da11e56cce0eb15756519d
Robbie Minshall <rjminsha@us.ibm.com>
2015-02-10 14:31:47 -0500

    Updating login logic

--- _init.sh
+++ _init.sh
@@ -34,6 +34,13 @@ export -f debugme
 set +e
 set +x 
 
+###############################
+# Configure extension PATH    #
+###############################
+if [ -n $EXT_DIR ]; then 
+    export PATH=$EXT_DIR:$PATH
+fi 
+
 ########################
 # REGISTRY INFORMATION #
 ########################
@@ -63,51 +70,79 @@ if [[ -z $BUILDER ]]; then
     #export BUILDER=http://50.22.19.253 
 fi
 
-########################
-# Bluemix information  #
-########################
-#  ice --verbose login --cf -H api-ice.stage1.ng.bluemix.net/ -R registry-ice.stage1.ng.bluemix.net/ --api api.stage1.ng.bluemix.net
-# CCS host url         : https://api-ice.stage1.ng.bluemix.net//v2/containers
-# Registry host        : registry-ice.stage1.ng.bluemix.net/
-# Bluemix api url      : api.stage1.ng.bluemix.net
-# Bluemix Org          : rjminsha@us.ibm.com (ea3dbb75-8f5d-4960-b3db-dd755e60ce9c)
-# Bluemix Space        : dev (570e8a76-a833-45b0-ad50-846947fc9da1)
+#################################
+# Set Bluemix Host Information  #
+#################################
 if [ -n "$BLUEMIX_TARGET" ]; then
     if [ "$BLUEMIX_TARGET" == "staging" ]; then 
         export CCS_API_HOST="api-ice.stage1.ng.bluemix.net" 
         export CCS_REGISTRY_HOST="registry-ice.stage1.ng.bluemix.net"
         export BLUEMIX_API_HOST="api.stage1.ng.bluemix.net"
-        if [ -z "$BLUEMIX_USER" ]; then 
-            echo -e "${red} Please set BLUEMIX_USER on environment ${no_color} "
-            exit 1
-        fi 
-        if [ -z "$BLUEMIX_PASSWORD" ]; then 
-            echo -e "${red} Please set BLUEMIX_PASSWORD on environment ${no_color} "
-            exit 1 
-        fi 
-        if [ -z "$BLUEMIX_ORG" ]; then 
-            export BLUEMIX_ORG=$BLUEMIX_USER
-            echo -e "${label_color}Defaulting bluemix org to $BLUEMIX_ORG, this can be overridden by setting BLUEMIX_ORG on the environment ${no_color}"
-        fi 
-        if [ -z "$BLUEMIX_SPACE" ]; then 
-            export BLUEMIX_SPACE="dev"
-            echo -e "${label_color} Defaulting bluemix org to $BLUEMIX_ORG, this can be overridden by setting BLUEMIX_ORG on the environment ${no_color}"
-
-        fi 
-
-        if [ $REGISTRY_SERVER == $CCS_REGISTRY_HOST ]; then 
-            echo "Targeting CCS_API_HOST ${CCS_API_HOST},CCS_REGISTRY_HOST ${CCS_REGISTRY_HOST}, ${BLUEMIX_API_HOST} "
-        else
-            echo -e "${red}Registry specified in target ( ${REGISTRY_SERVER} ) does not match the registry specified as a parameter ( ${CCS_REGISTRY_HOST} ) ${no_color}"  
-            exit 1
-        fi 
     else 
-        echo -e "${red}Unknown ${BLUEMIX_TARGET} specified"
+        echo -e "Targetting production bluemix"
+        echo -e "${label_color}TBD: read targetted environment from cf config.json${no_color}"
+        export CCS_API_HOST="api-ice.ng.bluemix.net" 
+        export CCS_REGISTRY_HOST="registry-ice.ng.bluemix.net"
+        export BLUEMIX_API_HOST="api.ng.bluemix.net"
     fi 
-else 
-    echo "Reading bluemix target environment from pipeline configuration"
-    echo -e "${label_color}TBD: load information from Cloud Fourndry credentials ${no_color}"
 fi 
+
+###################################
+# Get Bluemix Target Information  #
+###################################
+
+# If API_KEY is not provided get the org and space information 
+if [ -z "$API_KEY" ]; then 
+    pushd . 
+    cd ${EXT_DIR}
+    $(node cf_parser.js ~/.cf/config.json)
+    popd 
+    debugme echo "got org $CF_BLUEMIX_ORG from config.json" 
+    debugme echo "got space $CF_BLUEMIX_SPACE from config.json" 
+
+    if [ -z "$BLUEMIX_ORG" ]; then 
+        if [ -n $CF_BLUEMIX_ORG ]; then 
+            export BLUEMIX_ORG=$CF_BLUEMIX_ORG
+        elif [[ -z "$BLUEMIX_USER" ]]; then
+            export BLUEMIX_ORG=$BLUEMIX_USER
+        else 
+            echo -e "${red}Please set $BLUEMIX_USER and $BLUEMIX_ORG on the environment${no_color}"
+            exit 1
+        fi 
+        echo -e "${label_color} Using ${BLUEMIX_ORG} for Bluemix organization, please set BLUEMIX_ORG if on the environment if you wish to change this. ${no_color} "
+    fi 
+    if [ -z "$BLUEMIX_SPACE" ]; then
+        if [ -n "CF_BLUEMIX_SPACE" ]; then  
+            export BLUEMIX_SPACE=$CF_BLUEMIX_SPACE
+        else 
+            export BLUEMIX_SPACE="dev"
+        fi 
+        echo -e "${no_color} Using ${BLUEMIX_SPACE} for Bluemix space, please set BLUEMIX_SPACE if on the environment if you wish to change this. ${no_color} "
+    fi 
+fi 
+
+# Get the Bluemix user and password information 
+if [ -z "$BLUEMIX_USER" ]; then 
+    export BLUEMIX_USER="${CF_BLUEMIX_ORG}"
+    if [ -z "$BLUEMIX_USER" ]; then 
+        echo -e "${red} Please set BLUEMIX_USER on environment ${no_color} "
+        exit 1
+    else 
+        echo -e "${label_color} Using ${CF_BLUEMIX_ORG} as default user, please set BLUEMIX_USER on environment ${no_color} "
+    fi 
+fi 
+if [ -z "$BLUEMIX_PASSWORD" ]; then 
+    echo -e "${red} Please set BLUEMIX_PASSWORD as an environment property environment ${no_color} "
+    exit 1 
+fi 
+
+echo -e "${label_color}Targetting information.  Can be updated by setting environment variables${no_color}"
+echo "BLUEMIX_USER: ${BLUEMIX_SPACE}"
+echo "BLUEMIX_SPACE: ${BLUEMIX_SPACE}"
+echo "BLUEMIX_ORG: ${BLUEMIX_ORG}"
+echo "BLUEMIX_PASSWORD: xxxxx"
+echo ""
+
 
 ################################
 # Application Name and Version #
@@ -164,104 +199,94 @@ fi
 debugme echo "##################"
 debugme echo "installing ICE"
 debugme echo "##################"
-ice help 
+ice help >> ${LOG_DIR}/ice.log 2>&1 
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
     pushd . 
     cd $EXT_DIR
-    sudo apt-get -y install python2.7
-    python --version 
-    python get-pip.py --user
+    sudo apt-get -y install python2.7 >> ${LOG_DIR}/ice.log 2>&1
+    python get-pip.py --user >> ${LOG_DIR}/ice.log 2>&1
     export PATH=$PATH:~/.local/bin
-    pip --version 
-    # currently the 2.0 gives an error streaming output Defect 8153 
-    if [ -n "$API_KEY" ]; then 
-        debugme echo "Installing 1.0 CLI"
-        pip install --user icecli-1.0-0129.zip
-    else 
-        debugme echo "Installing 2.0 CLI"
-        pip install --user icecli-2.0.zip
-    fi 
-    ice help
+    pip install --user icecli-2.0.zip >> ${LOG_DIR}/ice.log 2>&1
+    ice help >> ${LOG_DIR}/ice.log 2>&1
     RESULT=$?
     if [ $RESULT -ne 0 ]; then
         echo -e "${red}Failed to install IBM Container Service CLI ${no_color}"
-        debugme echo -e "${label}Is python installed ${no_color}"
+        debugme more ${LOG_DIR}/ice.log 
         debugme python --version
-        debugme which python 
-        debugme echo $PATH
         exit $RESULT
-    fi 
+    fi
     popd 
+    debugme more ${LOG_DIR}/ice.log 
     echo -e "${label_color}Successfully installed IBM Container Service CLI ${no_color}"
 fi 
-######################
-# Check in CF        #
-######################
-cf help >> ${EXT_DIR}/init.log 2>&1 
+
+#############################
+# Install Cloud Foundry CLI #
+#############################
+debugme echo "#############################"
+debugme echo "# Install Cloud Foundry CLI #"
+debugme echo "#############################"
+echo -e "Cloud Foundry CLI not installed"
+pushd . 
+cd $EXT_DIR 
+gunzip cf-linux-amd64.tgz >> ${LOG_DIR}/cf.log 2>&1 
+tar -xvf cf-linux-amd64.tar >> ${LOG_DIR}/cf.log 2>&1 
+cf help >> ${LOG_DIR}/cf.log 2>&1 
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
-    echo -e "Cloud Foundry CLI not installed"
-    pushd . 
-    cd $EXT_DIR 
-    gunzip cf-linux-amd64.tgz
-    tar -xvf cf-linux-amd64.tar 
-    cf help >> ${EXT_DIR}/init.log 2>&1
-    RESULT=$?
-    if [ $RESULT -ne 0 ]; then
-        pwd 
-        ls 
-        popd
-        echo -e "${red}Could not install the cloud foundry CLI ${no_color}"
-        exit 1
-    else 
-        echo "Installed Cloud Foundry CLI"
-    fi
-    popd
-fi 
-
+    echo -e "${red}Could not install the cloud foundry CLI ${no_color}"
+    debugme more ${LOG_DIR}/cf.log
+    exit 1
+fi  
+popd
+debugme more ${LOG_DIR}/cf.log
+echo "Installed Cloud Foundry CLI"
 
 ################################
 # Login to Container Service   #
 ################################
 if [ -n "$API_KEY" ]; then 
     echo -e "${label_color}Logging on with API_KEY${no_color}"
-    ice login --key ${API_KEY}
+    ice login --key ${API_KEY} >> ${LOG_DIR}/login.log 2>&1
     RESULT=$?
 elif [[ -n "$BLUEMIX_TARGET" ]]; then
-     #statements 
-#        export CCS_API_HOST="api-ice.stage1.ng.bluemix.net" 
-#        export CCS_REGISTRY_HOST="api-ice.stage1.ng.bluemix.net"
-#        export BLUEMIX_API_HOST="api.stage1.ng.bluemix.net"
+    # User wants to specify all information 
     echo -e "${label_color}Logging via environment properties${no_color}"
-    
-    debugme echo "testing connectivity to services"
-    debugme ping -c 5 -t 10 ${CCS_API_HOST}
-    debugme ping -c 5 -t 10 ${BLUEMIX_API_HOST}
-    debugme ping -c 5 -t 10 ${CCS_REGISTRY_HOST}
-
     debugme echo "login command: ice --verbose login --cf -H ${CCS_API_HOST} -R ${CCS_REGISTRY_HOST} --api ${BLUEMIX_API_HOST}  --user ${BLUEMIX_USER} --psswd ${BLUEMIX_PASSWORD} --org ${BLUEMIX_ORG} --space ${BLUEMIX_SPACE}"
-    ice --verbose login --cf -H ${CCS_API_HOST} -R ${CCS_REGISTRY_HOST} --api ${BLUEMIX_API_HOST}  --user ${BLUEMIX_USER} --psswd ${BLUEMIX_PASSWORD} --org ${BLUEMIX_ORG} --space ${BLUEMIX_SPACE}
-    debugme more  /home/jenkins/.cf/config.json 
-    debugme more /home/jenkins/.ice/ice-cfg.ini
-    debugme ice info
+    ice login --cf -H ${CCS_API_HOST} -R ${CCS_REGISTRY_HOST} --api ${BLUEMIX_API_HOST}  --user ${BLUEMIX_USER} --psswd ${BLUEMIX_PASSWORD} --org ${BLUEMIX_ORG} --space ${BLUEMIX_SPACE} >> ${LOG_DIR}/login.log 2>&1
     RESULT=$?
 else 
-    echo -e "${label_color}TBD: support for token passed from pipeline via Cloud Foundry ${no_color}"
-    exit 1 
+    # User wants to login to production container service 
+    if [ -f "~/.cf/config.json" ]; then 
+        # we are already logged in.  Simply check via ice command 
+        echo -e "${label_color}Logging into IBM Container Service using credentials passed from IBM DevOps Services ${no_color}"
+        echo "checking login to api server" >> ${LOG_DIR}/login.log 2>&1
+        ice ps >> ${LOG_DIR}/login.log 2>&1
+        RESULT=$?
+        if [ $RESULT -ne 0 ]; then
+            echo "checking login to registry server" >> ${LOG_DIR}/login.log 2>&1
+            ice images >> ${LOG_DIR}/login.log 2>&1
+            RESULT=$? 
+        fi 
+    else 
+        # we need to login directly 
+        echo -e "${label_color}Logging into IBM Container Service${no_color}"
+            # User wants to specify all information 
+            echo -e "${label_color}Logging via environment properties${no_color}"
+            debugme echo "login command: ice --verbose login --cf -H ${CCS_API_HOST} -R ${CCS_REGISTRY_HOST} --api ${BLUEMIX_API_HOST}  --user ${BLUEMIX_USER} --psswd ${BLUEMIX_PASSWORD} --org ${BLUEMIX_ORG} --space ${BLUEMIX_SPACE}"
+            ice --verbose login --cf -H ${CCS_API_HOST} -R ${CCS_REGISTRY_HOST} --api ${BLUEMIX_API_HOST}  --user ${BLUEMIX_USER} --psswd ${BLUEMIX_PASSWORD} --org ${BLUEMIX_ORG} --space ${BLUEMIX_SPACE} >> ${LOG_DIR}/login.log 2>&1
+            RESULT=$?
+    fi 
 fi 
 
+debugme ice info 
+debugme more ${LOG_DIR}/login.log
+
+# check login result 
 if [ $RESULT -eq 1 ]; then
     echo -e "${red}Failed to login to IBM Container Service${no_color}"
     exit $RESULT
-fi 
-
-
-###############################
-# Configure extension PATH    #
-###############################
-if [ -n $EXT_DIR ]; then 
-    export PATH=$EXT_DIR:$PATH
 fi 
 
 ########################
