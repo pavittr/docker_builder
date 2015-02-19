c69a2b2d734e80e690de64c1ef7a9364350dc90f
Robbie Minshall <rjminsha@us.ibm.com>
2015-02-19 16:40:44 -0500

    Cleaning up debug

--- _init.sh
+++ _init.sh
@@ -250,21 +250,12 @@ else
     # we are already logged in.  Simply check via ice command 
     echo -e "${label_color}Logging into IBM Container Service using credentials passed from IBM DevOps Services ${no_color}"
     mkdir -p ~/.ice
-    echo "Copying ${EXT_DIR}/${ICE_CFG}"
     debugme cat "${EXT_DIR}/${ICE_CFG}"
     cp ${EXT_DIR}/${ICE_CFG} ~/.ice/ice-cfg.ini
-
-    debugme whoami 
-    pushd . 
-    cd ~ 
-    pwd 
-    popd 
-
     debugme cat ~/.ice/ice-cfg.ini
     debugme echo "config.json:"
     debugme cat /home/jenkins/.cf/config.json | cut -c1-2
     debugme cat /home/jenkins/.cf/config.json | cut -c3-
-
     ice --verbose ps > ps.log 
     debugme cat ps.log 
     RESULT=$?
