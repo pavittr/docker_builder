98543a7d807a3f461517966275f6518c8860639d
Bahram Sanaei <sanaei@us.ibm.com>
2015-11-17 13:28:48 -0500

    removed the last commit chnages for the spaces with spaces in the name

--- image_utilities.sh
+++ image_utilities.sh
@@ -55,18 +55,12 @@ if [ $IMAGE_LIMIT -gt 0 ]; then
                 # save current space first
                 $CFCMD target > target.log 2> /dev/null
                 debugme cat target.log 
-                #Use Show only matching chars option in grep to allow spaces in the current space name
-                CURRENT_SPACE=`grep "Space:" target.log | grep -o ":.*" | grep -o "[^: ].*"`
+                CURRENT_SPACE=`grep "Space:" target.log | awk '{print $2}'`
                 log_and_echo "$DEBUGGING" "current space is $CURRENT_SPACE"
                 FOUND=""
                 TESTED_ALL=true
-                #Build space array as an array to properly handle spaces in space names
-                SPACE_ARRAY=()
-                while read line; do 
-                    SPACE_ARRAY+=("$line"); 
-                done < inspect.log;
-                #Array needs to be in quotes to properly handle spaces in space names
-                for space in "${SPACE_ARRAY[@]}"
+                SPACE_ARRAY=$(cat inspect.log)
+                for space in ${SPACE_ARRAY[@]}
                 do
                     # cf spaces gives a couple lines of headers.  skip those until we find the line
                     # 'name', then read the rest of the lines as space names
@@ -76,7 +70,7 @@ if [ $IMAGE_LIMIT -gt 0 ]; then
                         fi
                         continue
                     else
-                        $CFCMD target -s "${space}" > target.log 2> /dev/null
+                        $CFCMD target -s ${space} > target.log 2> /dev/null
                         debugme cat target.log
                         if [ $? -eq 0 ]; then
                             log_and_echo "$DEBUGGING" "Checking space ${space}"
@@ -90,7 +84,7 @@ if [ $IMAGE_LIMIT -gt 0 ]; then
                     fi
                 done
                 # restore my old space
-                $CFCMD target -s "${CURRENT_SPACE}" > target.log 2> /dev/null
+                $CFCMD target -s ${CURRENT_SPACE} > target.log 2> /dev/null
                 debugme cat target.log
                 if [ "$TESTED_ALL" = true ] ; then
                     i=0
